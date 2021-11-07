957952d16968ce352d5d21d2507023f61693e5e8
YogiPw <76909923+yogipw@users.noreply.github.com>
2021-11-07 10:10:31 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -4,25 +4,28 @@ const multer = require('multer');
 const ejs = require('ejs');
 const path = require('path');
 
-BaseUrl = `https://file-uploader-js.herokuapp.com`
-LimitSize = 20 * 1024 * 1024 //20mb
+LimitSize = 50 * 1024 * 1024 //50mb
 
 // Set The Storage Engine
 const storage = multer.diskStorage({
   destination: './public/uploads/',
-  filename: function(req, file, cb){
-    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  filename: function(req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
   }
 });
 
 // Init Upload
 const upload = multer({
-  storage: storage, 
-  limits:{fileSize: LimitSize}
+  storage: storage,
+  limits: {
+    fileSize: LimitSize
+  }
 }).single('recfile');
 const upload2 = multer({
-  storage: storage, 
-  limits:{fileSize: LimitSize}
+  storage: storage,
+  limits: {
+    fileSize: LimitSize
+  }
 }).array('recfiles', 5);
 
 
@@ -39,12 +42,12 @@ app.get('/', (req, res) => res.render('index'));
 
 app.post('/upload', (req, res) => {
   upload(req, res, (err) => {
-    if(err){
+    if (err) {
       res.render('index', {
         msg: err
       });
     } else {
-      if(req.file == undefined){
+      if (req.file == undefined) {
         res.render('index', {
           msg: 'Error: No File Selected!'
         });
@@ -58,33 +61,39 @@ app.post('/upload', (req, res) => {
   });
 });
 app.post('/upload2', (req, res) => {
-  upload2(req, res, (err) => {
-Hehe = []
-    if(err){
-      res.render('index', {
-        msg2: err
-      });
-} else {
-      if(req.files == undefined){
+  upload2(req,
+    res,
+    (err) => {
+      Hehe = []
+      if (err) {
         res.render('index', {
-          msg2: 'Error: No File Selected!'
+          msg2: err
         });
       } else {
-        let x = req.files
-        x.map(async adm => {
-Hehe.push('https://' + req.hostname + '/uploads/' + adm.filename) 
-          
-        
-})
-res.json({
-          msg: 'File Uploaded!',
-          file: Hehe
-}) 
-      } 
-  }
-  });
-});
+        if (req.files == undefined) {
+          res.render('index', {
+            msg2: 'Error: No File Selected!'
+          });
+        } else {
+          let x = req.files
+          x.forEach(async v => {
+            Hehe.push({
+              originalname: v.originalname,
+              encoding: v.encoding,
+              mimetype: v.mimetype,
+              size: v.size,
+              url: "https://" + req.hostname + "/uploads/" + v.filename"
+            })
+            res.json({
+              msg: 'File Uploaded!',
+              file: Hehe
+            })
+          }
+          }
+        });
+    });
 
-const port = process.env.PORT;
+  const port = process.env.PORT;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+  app.listen(port,
+    () => console.log(`Server started on port ${port}`));
